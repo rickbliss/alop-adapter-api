0c6ec60ad48ed15808d6995819de31595306114d
rickbliss <rick.blss@gmail.com>
2018-04-02 09:43:55 -0400

    test

--- config/config.js
+++ config/config.js
@@ -1,7 +1,11 @@
 'use strict';
 
 module.exports = {
-  HOST: 'https://www.alotofpilates.com/',
-  API_TOKEN: 'MPP-Allow-API-Call'
+  /*HOST: 'https://www.alotofpilates.com/',
+  API_TOKEN: 'MPP-Allow-API-Call' */
+
+  HOST: 'http://alop-alop.1d35.starter-us-east-1.openshiftapps.com/',
+  API_TOKEN: 'MPP-Allow-API-Call' 
+
   
 };
